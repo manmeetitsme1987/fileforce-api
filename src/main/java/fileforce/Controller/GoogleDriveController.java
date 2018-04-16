ded75d192533f80c39fa02ecadab0a5ae4db72d2
Manmeet Manethiya <mmanethiya@mmanethiya-l1.local>
2018-04-16 15:43:42 +0530

    Handling request params from Salesforce

--- src/main/java/fileforce/Controller/GoogleDriveController.java
+++ src/main/java/fileforce/Controller/GoogleDriveController.java
@@ -1,11 +1,13 @@
 package fileforce.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import fileforce.Model.Request.GoogleDriveRequest;
 import fileforce.Model.Response.GoogleDriveFilesResponse;
 import fileforce.Service.GoogleDriveService;
 
@@ -16,7 +18,12 @@ public class GoogleDriveController {
     private GoogleDriveService gDriveService;
 	
 	@RequestMapping(value="/googleDriveData", method=RequestMethod.GET)
-	public @ResponseBody GoogleDriveFilesResponse getGoogleDriveData() {
-        return this.gDriveService.getGoogleDriveData(); 
+	public @ResponseBody GoogleDriveFilesResponse getGoogleDriveDataGet() {
+        return this.gDriveService.getGoogleDriveData(null); 
+    }
+	
+	@RequestMapping(value="/googleDriveData", method=RequestMethod.POST)
+	public @ResponseBody GoogleDriveFilesResponse getGoogleDriveDataPost(@RequestBody GoogleDriveRequest gDriveRequest) {
+        return this.gDriveService.getGoogleDriveData(gDriveRequest); 
     }
 }
